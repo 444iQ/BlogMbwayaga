90b01ee999ba8b399ee3f69e7dead494267fdc8f
444iQ <opompom24@gmail.com>
2024-11-05 19:14:23 +0300

    first commit

---
+++ localBlog.js
@@ -0,0 +1,19 @@
+alert('test ngori'); 
+   
+
+{
+    "posts"; [
+        {
+            "title": "My First Blog Post",
+            "summary": "The first blog post I've written on my blogging software!",
+            "post": "This is my first blog post content..."
+        }
+    ]
+
+}
+document.write("I always make it work!");
+    //todo: step1
+    //? what's next
+    //!what is this?     <script src="/Users/LENOVO T580/Desktop/ysot/WebProject/localBlog.js"></script>
+
+    //normal comment
